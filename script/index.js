6e0f7db5e6a9c7099819b55a06d5d312491d82ed
Ayesha <ayesha.glnt012@gmail.com>
2023-06-03 07:03:32 +0200

    list

--- script/index.js
+++ script/index.js
@@ -2,20 +2,24 @@ function add(){
     let todo = document.getElementById("todo")
     let display = document.getElementById("display")
         if(todo.value=== ''){
-            alert("You must write something!");
+            alert("Empty");
           }
           else if (todo.value.length < 3){
-            alert("Too Short")
+            alert("Type something else")
           }
-        //   else if (todo.value) {
-        //   }
+          else if (todo.value.length > 3) {
+            todo.value = todo.value.charAt(0).toUpperCase()
+            + todo.value.slice(1);
+          }
            else {
             let li = document.createElement("li")
-            // let checkbox = todo.value;
-            // checkbox.type = 'checkbox'
-            // checkbox.setAttribute('id', 'check')
             li.innerHTML = todo.value;
             display.appendChild(li);
           }
-        //   display.value = "";
+          display.value = "";
+    }
+
+    // function to delete
+    function del(){
+        display.value = display.value[0];
     }
