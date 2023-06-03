7cacdf36c9325257a1e4d73ace78b425938927e2
Ayesha <ayesha.glnt012@gmail.com>
2023-06-03 21:17:53 +0200

    more updates

--- script/index.js
+++ script/index.js
@@ -1,33 +1,51 @@
-let toDoList =[]
+
+// let toDoList = []
 
 function add(){
-    let todo = document.getElementById("todo")
+    let toDo = document.getElementById("todo")
     let display = document.getElementById("display")
-        if(todo.value === ''){
+        if(toDo.value === ''){
             alert("Enter something");
           }
-           else if (todo.value.length < 3){
+           else if (toDo.value.length < 3){
             alert("Type something else");
           }
-          else if (todo.value.length >= 3) {
-            todo.value = todo.value.charAt(0).toUpperCase()
-            + todo.value.slice(1);
-          }
+          // else if (toDo.value.length >= 3) {
+          //   toDo.value = display.value.charAt(0).toUpperCase()
+          //   + display.value.slice(1);
+          // }
            else {
-            let li = document.createElement("li")
-            li.innerHTML = todo.value;
-            display.appendChild(li);
             let checkbox = document.createElement('input')
             checkbox.type = 'checkbox'
             checkbox.setAttribute('id', 'check')
             display.append(checkbox); 
+            let li = document.createElement("li")
+            li.innerHTML = toDo.value;
+            display.appendChild(li);
             let button = document.createElement("button");
-            button.innerHTML = "❌",
+            button.innerHTML = "❌";
             display.appendChild(button);
           } 
           display.value = "";
-            console.log("hello"); 
     }
+
+
+    
+    // let todoArray = [];
+
+    // addTaskButton.addEventListener("click", (e) => {
+    //   e.preventDefault();
+    //   let todo = localStorage.getItem("todo");
+    //   if (todo === null) {
+    //     todoArray = [];
+    //   } else {
+    //     todoArray = JSON.parse(todo);
+    //   }
+    //   todoArray.push(text.value);
+    //   text.value = "";
+    //   localStorage.setItem("todo", JSON.stringify(todoArray));
+    //   displayTodo();
+    //  });
     // saveTasks();
 
     // function to sort into alphabetical order
@@ -48,7 +66,7 @@ function add(){
     // function save tasks to local sotrage
 
     // function saveTasks(){
-    //   // get all tasks from list using built-ins   map=loop arrays
+    // //   // get all tasks from list using built-ins   map=loop arrays
     //   const tasks = Array.from(display.value).map(item => item.textContent);
     //   localStorage.setItem('tasks', JSON.stringify(tasks))
     // }
